d7ac672c6bef769e3aa9e7bba049b61bba148a56
Vinicius Sousa <viniciusxsousa@outlook.com>
2023-02-10 19:21:48 -0300

    feat: finalizando comunicação com a API e iniciando a busca

--- pokedex/src/components/HeaderBusca/index.jsx
+++ pokedex/src/components/HeaderBusca/index.jsx
@@ -2,12 +2,12 @@ import { Cabecalho, CampoBusca } from './styles'
 
 import Logo from '../../assets/pokemon_logo.svg'
 
-function header () {
+function header ( {pesquisa} ) {
     return (
         <Cabecalho>
             <img src={Logo} alt="Logo do pokemon"/>
             <CampoBusca>
-                <input type="text" placeholder='Digite o nome do pokemon' />
+                <input onChange={(e) => pesquisa(e.target.value)} type="text" placeholder='Digite o nome do pokemon' />
                 <input type='submit' value='Buscar'/>
             </CampoBusca>
         </Cabecalho>
--- pokedex/src/pages/Resultados/index.jsx
+++ pokedex/src/pages/Resultados/index.jsx
@@ -1,18 +1,49 @@
 import HeaderBusca from '../../components/HeaderBusca'
 import Card from '../../components/Card/indes'
+import axios from 'axios'
 
 import { Conteiner, AreaFlex } from './styled'
+import { useEffect, useState } from 'react'
 
 function resultados() {
+
+    const [pokemons, setPokemons] = useState([]);
+
+    var urls = [];
+
+    for(var i = 1; i <= 20; i++){
+        urls.push(`https://pokeapi.co/api/v2/pokemon/${i}/`);
+    }
+
+    useEffect( () => {
+        axios.all(urls.map( (link) => axios.get(link) ))
+        .then((res) => setPokemons(res))
+        .catch((err) => console.log(err));
+
+        
+    }, []);
+    
+    const pokemonFiltro = (pesquisa) => {
+        var pokemonsFiltrados = [];
+
+        for( var i in pokemons) {
+            if(pokemons[i].name.include(pesquisa)) {
+                pokemonFiltro.push(pokemons[i]);
+            }
+        }
+
+        setPokemons[pokemonsFiltrados];
+    }
+
     return (
         <>
-            <HeaderBusca/>
+            <HeaderBusca pesquisa={pokemonFiltro}/>
             <Conteiner>
                 <h1>Resultados</h1>
                 <AreaFlex>
-                    <Card nome='Pikachu' img='https://picsum.photos/400/190' />
-                    <Card nome='pikachu' img='https://picsum.photos/190/190' />
-                    <Card nome='pikachu' img='https://picsum.photos/190/190' />
+                    {pokemons.map( (pokemon, key) => (
+                        <Card key={key} nome={pokemon.data.name} img={pokemon.data.sprites.front_default}/>
+                    ) )}
                 </AreaFlex>
             </Conteiner>
         </>
