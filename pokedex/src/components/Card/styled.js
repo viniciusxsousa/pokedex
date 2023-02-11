89e93065aa4568506d09048c63e91d997bf573fe
Vinicius Sousa <viniciusxsousa@outlook.com>
2023-02-11 12:34:36 -0300

    fix: corrigindo estilo do card

--- pokedex/src/components/Card/styled.js
+++ pokedex/src/components/Card/styled.js
@@ -5,7 +5,9 @@ export const CardSecao = styled.div`
     background-color: white;
     box-shadow: 0 2px 2px 0 rgb(0 0 0 / 14%), 0 3px 1px -2px rgb(0 0 0 / 12%), 0 1px 5px 0 rgb(0 0 0 / 20%);
     border-radius: 10px;
-
+    text-align: center;
+    cursor: pointer;
+    
     div {
         width: 100%;
     }
@@ -16,7 +18,6 @@ export const CardSecao = styled.div`
     }
 
     p {
-        text-align: center;
         font-size: 1.5em;
         padding: 0.5em 0;
     }
